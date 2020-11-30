a7bcc1d00d305a6ccee9241f016e9cc9254065ca
SirLanceALot1 <75269255+SirLanceALot1@users.noreply.github.com>
2020-11-30 10:19:21 -0700

    Add files via upload

---
+++ A simple GUI in Python/main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import pygame.mixer
+
+def play_correct_sound():
+   num_good.set(num_good.get() + 1)
+   correct_s.play()
+
+def play_wrong_sound():
+   num_bad.set(num_bad.get() + 1)
+   wrong_s.play()
+
+app = Tk()
+app.title("Basketball")
+app.geometry('300x110+200+100')
+
+sounds = pygame.mixer
+sounds.init()
+
+correct_s = sounds.Sound("correct.wav")
+wrong_s = sounds.Sound("wrong.wav")
+
+num_good = IntVar()
+num_good.set(0)
+num_bad = IntVar()
+num_bad.set(0)
+
+lab = Label(app, text='Do you like basketball?', height = 3)
+lab.pack()
+
+lab1 = Label(app, textvariable = num_good)
+lab1.pack(side = 'left')
+
+lab2 = Label(app, textvariable = num_bad)
+lab2.pack(side = 'right')
+
+b1 = Button(app, text = "Yes", width = 10, command = play_correct_sound)
+b1.pack(side = 'left', padx = 10, pady = 10)
+
+b2 = Button(app, text = "No", width = 10, command = play_wrong_sound)
+b2.pack(side = 'right', padx = 10, pady = 10)
+
+app.mainloop()
